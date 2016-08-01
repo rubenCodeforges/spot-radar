dea45398ca65ac7af331518fdb7e7cb1d62cc705
codeforges <ruben@codeforges.com>
2016-08-01 22:18:49 +0200

    SPOT-SERVER
    - REST Configuration
    - Auto routing generation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new Codeforges\SpotRadar\SpotApiBundle\SpotApiBundle(),
         );
 
---
+++ src/Codeforges/SpotRadar/SpotApiBundle/Controller/MarkerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Codeforges\SpotRadar\SpotApiBundle\Controller;
+
+
+use FOS\RestBundle\Controller\FOSRestController;
+
+class MarkerController extends FOSRestController
+{
+    public function getMarkersAction() {
+        
+    }
+    
+    public function getMarkerAction($id) {
+        
+    }
+}
