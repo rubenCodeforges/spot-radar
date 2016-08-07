724883497401c69811b93eda53c4c235a47e0ebd
rubenCodeforges <ruben@codeforges.com>
2016-08-07 20:41:14 +0200

    SPOT-4 oAuth setup
    - setup finished
    - modified client create command

--- src/Codeforges/SpotRadar/SpotApiBundle/Command/ClientCreateCommand.php
+++ src/Codeforges/SpotRadar/SpotApiBundle/Command/ClientCreateCommand.php
@@ -5,29 +5,69 @@ namespace Codeforges\SpotRadar\SpotApiBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class ClientCreateCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
         $this
-            ->setName('vp:oauth-server:client-create')
-            ->setDescription('Create a new client')
-            ->addArgument('name', InputArgument::REQUIRED, 'Sets the client name', null)
-            ->addOption('redirect-uri', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Sets redirect uri for client. Use this option multiple times to set multiple redirect URIs.', null)
-            ->addOption('grant-type', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Sets allowed grant type for client. Use this option multiple times to set multiple grant types.', null)
-        ;
+            ->setName('oauth:client:create')
+            ->setDescription('Create OAuth Client')
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'Client Name?'
+            )
+            ->addArgument(
+                'redirectUri',
+                InputArgument::REQUIRED,
+                'Redirect URI?'
+            )
+            ->addArgument(
+                'grantType',
+                InputArgument::REQUIRED,
+                'Grant Type?'
+            );
     }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $clientManager = $this->getContainer()->get('fos_oauth_server.client_manager.default');
+        $container = $this->getContainer();
+        $oauthServer = $container->get('fos_oauth_server.server');
+
+        $name = $input->getArgument('name');
+        $redirectUri = $input->getArgument('redirectUri');
+        $grantType = $input->getArgument('grantType');
+
+        $clientManager = $container->get('fos_oauth_server.client_manager.default');
         $client = $clientManager->createClient();
-        $client->setName($input->getArgument('name'));
-        $client->setRedirectUris($input->getOption('redirect-uri'));
-        $client->setAllowedGrantTypes($input->getOption('grant-type'));
+        $client->setName($name);
+        $client->setRedirectUris([$redirectUri]);
+        $client->setAllowedGrantTypes([$grantType]);
         $clientManager->updateClient($client);
-        $output->writeln(sprintf('Added a new client with name <info>%s</info> and public id <info>%s</info>.', $client->getName(), $client->getPublicId()));
+
+        $output->writeln(sprintf("<info>The client <comment>%s</comment> was created with <comment>%s</comment> as public id and <comment>%s</comment> as secret</info>",
+            $client->getName(),
+            $client->getPublicId(),
+            $client->getSecret()));
+        
+        //TODO: Add check for db driver type
+        $customers = $container->get('doctrine_mongodb')->getRepository('SpotApiBundle:Models\User')->findAll();
+
+        foreach ($customers as $customer) {
+            $queryData = [];
+            $queryData['client_id'] = $client->getPublicId();
+            $queryData['redirect_uri'] = $client->getRedirectUris()[0];
+            $queryData['response_type'] = 'code';
+            $authRequest = new Request($queryData);
+
+            $oauthServer->finishClientAuthorization(true, $customer, $authRequest, $grantType);
+
+            $output->writeln(sprintf("<info>User <comment>%s</comment> linked to client <comment>%s</comment></info>",
+                $customer->getId(),
+                $client->getName()));
+        }
     }
 }
